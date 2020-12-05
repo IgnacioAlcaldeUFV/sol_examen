d2e1560ef7fadc1abc0c4e04fe9f48e037255f12
Nacho <7736020@ufv.es>
2020-12-05 14:34:07 +0100

    Fixed LeeCSV class with return statement

--- src/main/java/org.dis/LeeCSV.java
+++ src/main/java/org.dis/LeeCSV.java
@@ -15,21 +15,22 @@ import java.util.List;
 public class LeeCSV {
 
     public List<golfPlayer> parseaCSV(String path) {
+        BufferedReader fileReader = null;
         try {
-            BufferedReader fileReader = new BufferedReader(new FileReader("best-golf-players.csv"));
+            fileReader = new BufferedReader(new FileReader("best-golf-players.csv"));
 
-            CsvToBean<golfPlayer> csvToBean = new CsvToBeanBuilder<golfPlayer>(fileReader) //Lista de beans para cargar el csv
-                    .withType(golfPlayer.class)
-                    .withIgnoreLeadingWhiteSpace(true) // Para ignorar blancos
-                    .build();
-
-            List<golfPlayer> players = csvToBean.parse();
-            return players;
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
+        CsvToBean<golfPlayer> csvToBean = new CsvToBeanBuilder<golfPlayer>(fileReader) //Lista de beans para cargar el csv
+                .withType(golfPlayer.class)
+                .withIgnoreLeadingWhiteSpace(true) // Para ignorar blancos
+                .build();
+
+        List<golfPlayer> players = csvToBean.parse();
+        return players;
 
     }
 }
